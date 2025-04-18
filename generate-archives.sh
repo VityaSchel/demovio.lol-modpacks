d0d2bdff6a1151bf6e6ea4dda9874f77994f8b0a
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-04-18 19:54:41 +0400

    Fix script

--- generate-archives.sh
+++ generate-archives.sh
@@ -1,45 +1,68 @@
+modpackver=v3
+
+echo "Creating modpacks archives for version $modpackver"
+
 rm -rf ./modpacks_archives
 mkdir modpacks_archives
 
-modpackver=v3
+tar -cf modpacks_archives/tmp_required_$modpackver.tar -C mods/required .
 
-tar -czvf modpack_archives/tmp_required_$modpackver.tar.gz -C mods/required .
+echo "[1/8] Base"
+cp modpacks_archives/tmp_required_$modpackver.tar modpacks_archives/base_$modpackver.tar
+tar -rf modpacks_archives/base_$modpackver.tar -C mods/base .
 
-# base
-cp modpacks_archives/tmp_required_$modpackver.tar.gz modpacks_archives/base_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base_$modpackver.tar.gz -C mods/base .
+echo "[2/8] Base+Optimizations"
+cp modpacks_archives/base_$modpackver.tar modpacks_archives/base+optimizations_$modpackver.tar
+tar -rf modpacks_archives/base+optimizations_$modpackver.tar -C mods/optimizations .
 
-# base, optimizations
-cp modpacks_archives/base_$modpackver.tar.gz modpacks_archives/base+optimizations_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base+optimizations_$modpackver.tar.gz -C mods/optimizations .
+echo "[2/8] Base+Extras"
+cp modpacks_archives/base_$modpackver.tar modpacks_archives/base+extras_$modpackver.tar
+tar -rf modpacks_archives/base+extras_$modpackver.tar -C mods/extras .
 
-# base, extras
-cp modpacks_archives/base_$modpackver.tar.gz modpacks_archives/base+extras_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base+extras_$modpackver.tar.gz -C mods/extras .
+echo "[3/8] Base+Visual"
+cp modpacks_archives/base_$modpackver.tar modpacks_archives/base+visual_$modpackver.tar
+tar -rf modpacks_archives/base+visual_$modpackver.tar -C mods/visual .
 
-# base, visual
-cp modpacks_archives/base_$modpackver.tar.gz modpacks_archives/base+visual_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base+visual_$modpackver.tar.gz -C mods/visual .
+echo "[4/8] Base+Optimizations+Extras"
+cp modpacks_archives/base_$modpackver.tar modpacks_archives/base+optimizations+extras_$modpackver.tar
+tar -rf modpacks_archives/base+optimizations+extras_$modpackver.tar -C mods/optimizations .
+tar -rf modpacks_archives/base+optimizations+extras_$modpackver.tar -C mods/extras .
 
-# base, optimizations, extras
-cp modpacks_archives/base_$modpackver.tar.gz modpacks_archives/base+optimizations+extras_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base+optimizations+extras_$modpackver.tar.gz -C mods/optimizations .
-tar -czrvf modpacks_archives/base+optimizations+extras_$modpackver.tar.gz -C mods/extras .
+echo "[5/8] Base+Optimizations+Visual"
+cp modpacks_archives/base_$modpackver.tar modpacks_archives/base+optimizations+visual_$modpackver.tar
+tar -rf modpacks_archives/base+optimizations+visual_$modpackver.tar -C mods/optimizations .
+tar -rf modpacks_archives/base+optimizations+visual_$modpackver.tar -C mods/visual .
 
-# base, optimizations, visual
-cp modpacks_archives/base_$modpackver.tar.gz modpacks_archives/base+optimizations+visual_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base+optimizations+visual_$modpackver.tar.gz -C mods/optimizations .
-tar -czrvf modpacks_archives/base+optimizations+visual_$modpackver.tar.gz -C mods/visual .
+echo "[6/8] Base+Extras+Visual"
+cp modpacks_archives/base_$modpackver.tar modpacks_archives/base+extras+visual_$modpackver.tar
+tar -rf modpacks_archives/base+extras+visual_$modpackver.tar -C mods/extras .
+tar -rf modpacks_archives/base+extras+visual_$modpackver.tar -C mods/visual .
 
-# base, extras, visual
-cp modpacks_archives/base_$modpackver.tar.gz modpacks_archives/base+extras+visual_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base+extras+visual_$modpackver.tar.gz -C mods/extras .
-tar -czrvf modpacks_archives/base+extras+visual_$modpackver.tar.gz -C mods/visual .
+echo "[7/8] Base+Optimizations+Extras+Visual"
+cp modpacks_archives/base_$modpackver.tar modpacks_archives/base+optimizations+extras+visual_$modpackver.tar
+tar -rf modpacks_archives/base+optimizations+extras+visual_$modpackver.tar -C mods/optimizations .
+tar -rf modpacks_archives/base+optimizations+extras+visual_$modpackver.tar -C mods/extras .
+tar -rf modpacks_archives/base+optimizations+extras+visual_$modpackver.tar -C mods/visual .
 
-# base, optimizations, extras, visual
-cp modpacks_archives/base_$modpackver.tar.gz modpacks_archives/base+optimizations+extras+visual_$modpackver.tar.gz
-tar -czrvf modpacks_archives/base+optimizations+extras+visual_$modpackver.tar.gz -C mods/optimizations .
-tar -czrvf modpacks_archives/base+optimizations+extras+visual_$modpackver.tar.gz -C mods/extras .
-tar -czrvf modpacks_archives/base+optimizations+extras+visual_$modpackver.tar.gz -C mods/visual .
+printf '%s' "[8/8] Compressing... "
 
-rm tmp_required_$modpackver.tar.gz
+gzip -f modpacks_archives/base_$modpackver.tar
+printf '%s' "12%... "
+gzip -f modpacks_archives/base+optimizations_$modpackver.tar
+printf '%s' "25%... "
+gzip -f modpacks_archives/base+extras_$modpackver.tar
+printf '%s' "37%... "
+gzip -f modpacks_archives/base+visual_$modpackver.tar
+printf '%s' "50%... "
+gzip -f modpacks_archives/base+optimizations+extras_$modpackver.tar
+printf '%s' "62%... "
+gzip -f modpacks_archives/base+optimizations+visual_$modpackver.tar
+printf '%s' "75%... "
+gzip -f modpacks_archives/base+extras+visual_$modpackver.tar
+printf '%s' "87%... "
+gzip -f modpacks_archives/base+optimizations+extras+visual_$modpackver.tar
+echo "100%"
+
+rm modpacks_archives/tmp_required_$modpackver.tar
+
+echo "Operation finished: ./modpacks_archives/"
